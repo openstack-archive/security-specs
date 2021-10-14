b3dcc7a310863e0c2f78e69adbd00528689e9a13
Hervé Beraud <hberaud@redhat.com>
2021-10-14 14:57:46 +0200

    Restore reproducibility in docs
    The documentation is writting the date in its copyright. This is
    considered bad practice, because this kills reproducibility. If
    you don't know about reproducible build, please read on:
    
    https: //reproducible-builds.org/
    Change-Id: I6199d139e3c8334b7795bdb4875553c613adcec7

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -67,7 +67,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'security-specs'
-copyright = '%s, OpenStack Foundation' % datetime.date.today().year
+copyright = 'OpenStack Foundation'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = True
